f2cc7754707d3ebdd93fab40391c88e2cb254254
Keroisasleep <krenzgeromerobiso@gmail.com>
2025-07-14 22:37:49 +0800

    ulit

--- lib/main.dart
+++ lib/main.dart
@@ -2,34 +2,82 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-              title: const Text('GALABUDDY', style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-
-              ),),
-              backgroundColor: Colors.grey[500]
-          ),
-          body: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('KERO1', style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w300
-                  ),),
-                  Text('KERO2', style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.w600
-                  ),),
-                  Text('KERO3', style: TextStyle(
-                      fontSize: 60,
-                      fontWeight: FontWeight.w900
-                  ),),
-                ]
-            ),
-          )
-      )
+    home: Profile(),
+    debugShowCheckedModeBanner: false,
   ));
+}
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GALA BUDDY', style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,),),
+        backgroundColor: Colors.grey,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      Text(
+                        'User Profile',
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 10),
+                      Icon(Icons.person, size: 100),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30),
+                Text(
+                  'Name',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text('Krenz Gerome E. Robiso'),
+                SizedBox(height: 20),
+                Text(
+                  'Email',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text('22-08796@g.batstate-u.edu.ph'),
+                SizedBox(height: 20),
+                Text(
+                  'Company',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text('Batangas State University TNEU Balayan'),
+                SizedBox(height: 20),
+                Text(
+                  'Contact Number',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text('09294720019'),
+              ],
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Log out logic here
+                },
+                child: Text('Log Out'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
